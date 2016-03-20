fc74ab7871c8619d6b7bdbf32bb91c7d6f47e458
dansancarp <dansan0012@gmail.com>
2016-03-19 22:38:31 -0300

    Primer commit repaso laravel5.2

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Task;
+
+use App\Http\Requests\TaskRequest;
+
+class TaskController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        $tasks = Task::where('user_id', $request->user()->id)->get();
+        return view('tasks.index',['tasks'=>$tasks]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('tasks.new');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(TaskRequest $request)
+    {        
+        $request->user()->tasks()->create($request->all());
+        return redirect('/task')->with('success','Tarea creada correctamente');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Task $task)
+    {
+        return view('tasks.edit',[
+                'task' => $task,
+            ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Task $task)
+    {
+        $this->authorize('owner',$task);
+        $task->update($request->all());
+        return redirect('/task')->with('success','Tarea editada correctamente');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ resources/views/tasks/form.blade.php
@@ -0,0 +1,4 @@
+{!!Form::label('name','Nombre: ')!!}
+		{!!Form::text('name',null,['class'=>'form-control'])!!}
+
+		
